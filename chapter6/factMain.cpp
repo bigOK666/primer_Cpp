51089edcff302c8818f024de1102e79fda4d5619
Lu <zeren.lu@gmail.com>
2021-02-08 16:04:40 +0100

    seperate compile

--- chapter6/factMain.cpp
+++ chapter6/factMain.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	std::cout << fact_interact << std::endl;
+	std::cout << fact_interact() << std::endl;
 	std::cout << fact(5) << std::endl;
 	std::cout << abs_test(-5) << std::endl;
 	return 0;
