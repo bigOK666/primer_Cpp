0266ec09674c87fa11b1295b7ad55a88c0408c47
Lu <zeren.lu@gmail.com>
2021-02-15 09:52:20 +0100

    combine

--- chapter7/exercise7_2.cpp
+++ chapter7/exercise7_2.cpp
@@ -8,3 +8,10 @@ struct Sales_data {
 	std::string isbn() const { return bookNo; };
 	Sales_data& combine(const Sales_data&);
 };
+//返回类型为左侧对象的引用，返回类型为引用不需要在return中做什么，而是在返回类型上加入&
+Sales_data& Sales_data::combine(const Sales_data& rhs)
+{
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
