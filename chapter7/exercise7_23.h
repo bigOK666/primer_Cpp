fe2dc30bbd1de0242dba94ca525577a7540e9515
Lu <zeren.lu@gmail.com>
2021-02-19 11:41:16 +0100

    constructor and inline member function

---
+++ chapter7/exercise7_23.h
@@ -0,0 +1,32 @@
+#include<string>
+
+class Screen {
+public:
+	typedef std::string::size_type pos;
+
+	Screen() = default;
+	Screen(pos h, pos w) :height(h), width(w), contents(h* w, ' ') {};
+	Screen(pos h, pos w, char c) : height(h), width(w), contents(h* w, c) {};
+
+	char get() const { return contents[cursor]; }
+	char get(pos r, pos c) const;
+	Screen& move(pos r, pos c);
+
+private:
+	pos cursor = 0;
+	pos height = 0, width = 0;
+	std::string contents;
+};
+
+inline Screen& Screen::move(pos r, pos c)
+{
+	pos row = r * width;
+	cursor = row + c;
+	return *this;
+}
+
+inline char Screen::get(pos r, pos c) const
+{
+	pos row = r * width;
+	return contents[row + c];
+}
