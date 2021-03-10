6a8fc6832a7f191161abc2575a13376ee450b6f3
Lu <zeren.lu@gmail.com>
2021-03-10 14:59:43 +0100

    understand 3/5 principle: the relationship between copy, assign and destructor

---
+++ chapter13/exercise13_14.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+class numbered {
+public:
+	numbered() { ++mysn; }
+
+	int mysn = 0;
+};
+
+void f(numbered s) { std::cout << s.mysn << std::endl; }
+
+int main()
+{
+	numbered a, b = a, c = b;
+	f(a); f(b); f(c);
+}
