a8b28b24127472fcff0c7e76e14956e84365d0be
Lu <zeren.lu@gmail.com>
2021-03-14 12:08:29 +0100

    重载运算符

---
+++ chapter14/exercise14_5.h
@@ -0,0 +1,21 @@
+#include<string>
+#include<iostream>
+class Employee {
+
+	friend std::ostream& operator<<(std::ostream& os, const Employee&);
+	Employee() :Employee("", "", 0) {};
+	Employee(const std::string& n, const std::string& d, double s):name(n), department(d), salary(0) {};
+
+	
+private:
+	std::string name;
+	std::string department;
+	double salary = 0;
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Employee& e)
+{
+	std::cout << e.name << " " << e.department << " " << e.salary << std::endl;
+	return os;
+}
