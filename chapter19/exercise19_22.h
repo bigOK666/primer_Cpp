46c0096365ee9862d64f5c4de6d8347a488bfd1c
Lu <zeren.lu@gmail.com>
2021-04-03 14:56:18 +0200

    union中含有类的时候就复杂了

---
+++ chapter19/exercise19_22.h
@@ -0,0 +1,107 @@
+#include<string>
+class Sales_data {
+
+public:
+	Sales_data() = default;
+	double avg_price() const { return revenue / units_sold; }
+	~Sales_data() = default;
+private:
+	std::string bookNo;
+	unsigned units_sold;
+	double revenue;
+};
+class Token {
+public:
+	Token() :tok(INT), ival(0) {}
+	Token(const Token& t) :tok(t.tok) { copyUnion(t); }
+	Token& operator=(const Token&);
+	~Token() { if (tok == STR) { sval.std::string::~string(); } else if (tok == SD) { sdval.~Sales_data(); } }
+	Token& operator=(const std::string&);
+	Token& operator=(char);
+	Token& operator=(int);
+	Token& operator=(double);
+	Token& operator=(const Sales_data&);
+private:
+	enum { INT, CHAR, DBL, STR, SD} tok;
+	union {
+		char cval;
+		int ival;
+		double dval;
+		std::string sval;
+		Sales_data sdval;
+	};
+	void copyUnion(const Token&);
+};
+
+Token& Token::operator=(int i)
+{
+	if (tok == STR) sval.std::string::~string();
+	ival = i;
+	tok = INT;
+	return *this;
+}
+
+Token& Token::operator=(char c)
+{
+	if (tok == STR) sval.std::string::~string();
+	cval = c;
+	tok = CHAR;
+	return *this;
+
+}
+
+Token& Token::operator=(double d)
+{
+	if (tok == STR) sval.std::string::~string();
+	dval = d;
+	tok = DBL;
+	return *this;
+}
+
+Token& Token::operator=(const std::string& s)
+{
+	if (tok == STR)
+		sval = s;
+	else
+		new(&sval) std::string(s);
+	tok = STR;
+	return *this;
+}
+
+Token& Token::operator=(const Sales_data& sd)
+{
+	if(tok == STR) sval.std::string::~string();
+	if (tok == SD)
+		sdval = sd;
+	else
+		new(&sdval) Sales_data(sd);
+
+	tok = SD;
+	return *this;
+		
+		
+}
+
+void Token::copyUnion(const Token& t)
+{
+	switch (t.tok) {
+	case Token::INT: ival = t.ival; break;
+	case Token::CHAR: cval = t.cval; break;
+	case Token::DBL: dval = t.dval; break;
+	case Token::STR: new(&sval) std::string(t.sval); break;
+	case Token::SD: new(&sdval) Sales_data(t.sdval); break;
+	}
+}
+
+Token& Token::operator=(const Token& t)
+{
+	if (tok == STR && t.tok != STR) sval.std::string::~string();
+	if (tok == SD && t.tok != SD) sdval.~Sales_data();
+	if (tok == SD && t.tok == SD) sdval = t.sdval;
+	if (tok == STR && t.tok == STR)
+		sval = t.sval;
+	else
+		copyUnion(t);
+	tok = t.tok;
+	return *this;
+}
---
+++ chapter19/exercise19_25.cpp
@@ -0,0 +1,10 @@
+#include"exercise19_23.h"
+
+int main()
+{
+	Token t;
+	std::string s{ "Test" };
+	t = s;
+	t = 5;
+	t = 2.4;
+}
